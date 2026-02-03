28f3218828ad407b7339e617e5ba08d3f41f9fa6
erikadeolima <erikadeo.lima@hotmail.com>
2026-02-03 20:25:07 -0300

    Refactor code structure and remove redundant code blocks for improved readability and maintainability

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace controle_estoque_cshap.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
