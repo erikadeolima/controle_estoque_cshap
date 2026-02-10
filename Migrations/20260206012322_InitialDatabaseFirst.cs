81940b4ece23bbe350eeecab15976a9fec30aacb
Erika Lima <erikadeo.lima@hotmail.com>
2026-02-10 21:44:18 +0000

    Remover tabela __EFMigrationsHistory da migração inicial do banco de dados

--- Migrations/20260206012322_InitialDatabaseFirst.cs
+++ Migrations/20260206012322_InitialDatabaseFirst.cs
@@ -16,22 +16,6 @@ namespace controle_estoque_cshap.Migrations
                 .Annotation("MySql:CharSet", "utf8mb4");
 
             migrationBuilder.CreateTable(
-                name: "__EFMigrationsHistory",
-                columns: table => new
-                {
-                    MigrationId = table.Column<string>(type: "varchar(150)", maxLength: 150, nullable: false, collation: "utf8mb4_0900_ai_ci")
-                        .Annotation("MySql:CharSet", "utf8mb4"),
-                    ProductVersion = table.Column<string>(type: "varchar(32)", maxLength: 32, nullable: false, collation: "utf8mb4_0900_ai_ci")
-                        .Annotation("MySql:CharSet", "utf8mb4")
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PRIMARY", x => x.MigrationId);
-                })
-                .Annotation("MySql:CharSet", "utf8mb4")
-                .Annotation("Relational:Collation", "utf8mb4_0900_ai_ci");
-
-            migrationBuilder.CreateTable(
                 name: "category",
                 columns: table => new
                 {
@@ -180,9 +164,6 @@ namespace controle_estoque_cshap.Migrations
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "__EFMigrationsHistory");
-
             migrationBuilder.DropTable(
                 name: "movement");
 
