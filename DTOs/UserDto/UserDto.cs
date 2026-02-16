df19fe037a619e77582064d7fd8467c56fbf9d1c
erikadeolima <erikadeo.lima@hotmail.com>
2026-02-16 20:21:46 -0300

    Implement user management functionality: add UserController, UserService, UserRepository, and DTOs for user creation and retrieval

---
+++ DTOs/UserDto/UserDto.cs
@@ -0,0 +1,9 @@
+namespace controle_estoque_cshap.DTOs.UserDto;
+
+public class UserDto
+{
+  public int IdUser { get; set; }
+  public string Name { get; set; } = string.Empty;
+  public string Email { get; set; } = string.Empty;
+  public string Profile { get; set; } = string.Empty;
+}
